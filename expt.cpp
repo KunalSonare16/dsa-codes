c69e04f623bbf34deffb840a67871592ffbb39a5
KunalSonare16 <kunalsonare28@gmail.com>
2025-05-23 19:17:04 +0530

    ll

--- expt.cpp
+++ expt.cpp
@@ -1,29 +1,76 @@
 #include<iostream>
-#include<math.h>
-#include<list>
 using namespace std;
 
-int main(){
-  int n,m;
-  cin>>n>>m;
-  int **arr = new int*[n];
-  for(int i = 0 ; i<m ;i++){
-     arr[i] = new int[m];
-  }
-  for(int i = 0 ; i < n ; i ++){
-    for(int j = 0 ; j < m ; j++){
-      cin>>arr[i][j];
+class node {
+public:
+    int data;
+    node* next;
+    node(int n) {
+        this->data = n;
+        this->next = NULL;
     }
-  }
-for(int i = 0 ; i < n ; i ++){
-    for(int j = 0 ; j < m ; j++){
-      cout<<arr[i][j];
-    }
-  }
-  for( int i = 0 ; i < m ; i++){
-    delete [] arr[i];
+};
 
-  }
-  delete []  arr;
-  return 0 ;
+void insertionathead(node* &head, int d) {
+    node* temp = new node(d);
+    temp->next = head;
+    head = temp;
+}
+
+void insertionattale(node* &tale, int d) {
+    node* temp = new node(d);
+    tale->next = temp;
+    tale = temp;
+}
+
+void insertatmiddle(node* &tale, node* &head, int position, int d) {
+    if (position == 1) {
+        insertionathead(head, d);
+        return;
+    }
+
+    node* temp = head;
+    int count = 1;
+
+    while (count < position - 1) {
+        if (temp->next == NULL) {
+            insertionattale(tale, d);  // Insert at tail if position is beyond current length
+            return;
+        }
+        temp = temp->next;
+        count++;
+    }
+
+    node* tempe = new node(d);
+    tempe->next = temp->next;
+    temp->next = tempe;
+
+    if (tempe->next == NULL) {
+        tale = tempe;  // Update tail if new node is now the last
+    }
+}
+
+void printll(node* head) {
+    node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    node* n1 = new node(30);
+    node* head = n1;
+    node* tale = n1;
+
+    insertatmiddle(tale, head, 1, 10);  // Insert at head
+    insertatmiddle(tale, head, 2, 20);  // Insert in middle
+    insertatmiddle(tale, head, 4, 40);  // Insert at tail
+
+    cout << "Head: " << head->data << endl;
+    cout << "Tail: " << tale->data << endl;
+    printll(head);  // Output: 10 20 30 40
+
+    return 0;
 }
