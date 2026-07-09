881627e8bbdb0022413eb8e1f78e72c581462ed9
Kunal Sonare <kunalsonare28@gmail.com>
2026-07-09 07:20:08 +0530

    Time: 3 ms (53.03%), Space: 14.1 MB (58.3%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
- void solve( int index , vector<vector<int>> &ans , vector<int> &arr, int target , vector<int> &kk){
-       if(target == 0 ){
-           ans.push_back(kk);
-           return;
-       }
-       if(index >= arr.size() || target<0) return;
-     
-           kk.push_back(arr[index]);
-           solve(index,ans,arr,target-arr[index],kk);
-           kk.pop_back();
-           
-           
-         solve(index+1,ans,arr,target,kk);
-       
-   }
+    void solve(int index, vector<vector<int>> &ans, vector<int> &arr, int target, vector<int> &kk) {
+        if (target == 0) {
+            ans.push_back(kk);
+            return;
+        }
+
+        if (index >= arr.size() || target < 0)
+            return;
+
+        for (int i = index; i < arr.size(); i++) {
+            kk.push_back(arr[i]);
+            solve(i, ans, arr, target - arr[i], kk); 
+            kk.pop_back();
+        }
+    }
+
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-         vector<vector<int>> ans;
-      vector<int> kk;
-      solve(0,ans,candidates,target,kk);
-      
-      return ans;
-        
+        vector<vector<int>> ans;
+        vector<int> kk;
+
+        solve(0, ans, candidates, target, kk);
+
+        return ans;
     }
 };
